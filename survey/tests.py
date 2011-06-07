6ff9537dc1918a4e787130ed5b0ccda17f7b5d63
Santhosh Balasubramanian <santhoshbala@gmail.com>
2011-06-06 20:14:59 -0400

    bug fix django version 1.3

--- survey/tests.py
+++ survey/tests.py
@@ -1,16 +1,65 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+import datetime
 from django.test import TestCase
+from django.db import IntegrityError
+from survey.models import Survey
 
+class SurveyUnicodeTest(TestCase):
+    def testUnicode(self):
+        t = u'Como esta usted?'
+        sd = datetime.date(2009, 12, 28)
+        s = Survey.objects.create(title=t, opens=sd)
+        self.assertEqual(unicode(s),
+                u'Como esta usted? (opens 2009-12-28, closes 2010-01-04)')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from survey.models import Question
+class QuestionWinningAnswersTest(TestCase):
+    
+    fixtures = ['test_winning_answers.json']
+    
+    def testClearWinner(self):
+        q = Question.objects.get(question='Clear Winner')
+        wa_qs = q.winning_answers()
+        self.assertEqual(wa_qs.count(), 1)
+        winner = wa_qs[0]
+        self.assertEqual(winner.answer, 'Max Votes')
+        
+class SurveySaveTest(TestCase):
+    t = "New Year's Resolutions"
+    sd = datetime.date(2009, 12, 28)
+    
+    def testClosesAutoset(self):
+        s = Survey.objects.create(title=self.t, opens=self.sd)
+        self.assertEqual(s.closes, datetime.date(2010,1,4))
+        
+    def testClosesHonored(self):
+        s = Survey.objects.create(title=self.t, opens=self.sd,closes=self.sd)
+        self.assertEqual(s.closes, self.sd)
+        
+    def testClosesReset(self):
+        s = Survey.objects.create(title=self.t, opens=self.sd)
+        s.closes=None
+        
+        strict = True
+        debug = False
+        from django.conf import settings
+        if 'mysql' in settings.DATABASES.get('default').get('ENGINE'):
+            from django.db import connection
+            c = connection.cursor()
+            c.execute('SELECT @@SESSION.sql_mode')
+            mode = c.fetchone()[0]
+            if 'STRICT' not in mode:
+                strict = False;
+                from django.utils import importlib
+                debug = importlib.import_module(
+                        settings.SETTINGS_MODULE).DEBUG
+                
+        if strict:
+            self.assertRaises(IntegrityError, s.save)
+        elif debug:
+            self.assertRaises(Exception, s.save)
+        else:
+            s.save()
+            self.assertEqual(s.closes, None)
+    
+    def testTitleOnly(self):
+        self.assertRaises(IntegrityError, Survey.objects.create,title=self.t)
